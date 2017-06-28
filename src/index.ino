dbf0a3f173d9b6a565241fb9c6f3f7bae2111a72
judsonc <judson.scosta@hotmail.com>
2017-06-28 03:15:34 -0300

    Alguns comentarios

--- src/index.ino
+++ src/index.ino
@@ -8,12 +8,12 @@
 // DEFINES
 //===================================================================================
 
-#define EMITPOSTLOG "/post/log/" //rota de envio
+#define EMITPOSTLOG "/post/log/" // rota de envio
 #define TIMETOEMIT 1 // tempo de envio em segundos
-#define HOST "192.168.1.67" // host de envio
-#define PORT 3001 // porta para envio
+#define HOST "192.168.1.52" // host de envio
+#define PORT 80 // porta para envio
 #define SENSORLUZ A0 // porta onde os sinais estao sendo recebidos
-#define NOME "Luz da sala" // nome do dispositivo
+#define NOME "Sala" // nome do dispositivo
 
 //===================================================================================
 // VARIABLES
@@ -39,10 +39,10 @@ void toEmit(void) {
 
 void setup() {
   Serial.begin(115200); // inicia o serial
-  pinMode(SENSORLUZ, INPUT_PULLUP); // seta a porta de leitura
+  pinMode(SENSORLUZ, INPUT_PULLUP); // seta a porta do sensor
   sending.attach(TIMETOEMIT, toEmit); // interrupcao por tempo
-  wifi.autoConnect();
-  socket.begin(HOST, PORT);
+  wifi.autoConnect(); // conectar no ultimo wifi salvo
+  socket.begin(HOST, PORT); // inicia a conexão com o servidor
 }
 
 //===================================================================================
