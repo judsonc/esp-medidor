7a8b4ba5e4e2a2438d511230f6a7a0c2711f85d8
judsonc <judson.scosta@hotmail.com>
2017-06-28 03:45:58 -0300

    Device com valores default e aceita apenas um pulso 0

--- server/model/Device.js
+++ server/model/Device.js
@@ -1,25 +1,24 @@
 class Device {
   constructor(body) {
-    this.nome = body.nome
-    this.valor = body.valor
-    this.contador = body.contador
-    this.acumulado = body.acumulado
-    this.media = body.media
-    this.data_hora = body.data_hora
+    this.nome = body.nome || new Date().getTime()
+    this.valor = body.valor || 0
+    this.contador = body.contador || 0
+    this.acumulado = body.acumulado || 0
+    this.media = body.media || 0
+    this.data_hora = new Date().toISOString()
   }
 
   addValor(valor) {
-    if (1024 - valor) {
+    if ((1024 - valor) || this.valor) {
       this.contador += 1
       this.acumulado += valor
-      this.data_hora = new Date().toISOString()
       this.valor = Number((((1024 - valor)/1024)*100).toFixed(1))
       this.media = Number((((1024 - (this.acumulado/this.contador))/1024)*100).toFixed(1))
     }
   }
 
   set(body) {
-    this.nome = body.nome || 'None'
+    this.nome = body.nome || this.nome
     this.addValor(body.valor)
   }
 }
